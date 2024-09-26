d822d3b87d85ba01ff5c25619d33894ee82ebc49
Alexis RARCHAERT <bonjour@alexis-rarchaert.fr>
2024-09-26 21:17:57 +0200

    chore: Add ical.js library to package.json

--- index.js
+++ index.js
@@ -1,46 +1,60 @@
+//Importation des librairies
 require('dotenv').config();
-const { google } = require('googleapis');
-const { OAuth2 } = google.auth;
-const calendar = google.calendar('v3');
 const { Client } = require('@notionhq/client');
+const ICAL = require('ical.js');
 
-const oAuth2Client = new OAuth2(
-  process.env.GOOGLE_CLIENT_ID,
-  process.env.GOOGLE_CLIENT_SECRET
-);
-oAuth2Client.setCredentials({
-  refresh_token: process.env.GOOGLE_REFRESH_TOKEN
-});
-
+// On créé un client Notion
 const notion = new Client({ auth: process.env.NOTION_TOKEN });
-const databaseCoursId = process.env.NOTION_DATABASE_COURS_ID;
-const databaseMealsId = process.env.NOTION_DATABASE_MEALS_ID;
 
-async function getUpcomingEvents() {
-  const res = await calendar.events.list({
-    auth: oAuth2Client,
-    calendarId: process.env.GOOGLE_CALENDAR_ID,
-    timeMin: (new Date()).toISOString(),
-    maxResults: 15,
-    singleEvents: true,
-    orderBy: 'startTime',
-  });
-  return res.data.items.reverse();
+async function getUpcomingEventsForMoodle(calendarUrl) {
+  const res = await fetch(calendarUrl);
+  const icalData = await res.text();
+
+  const jcalData = ICAL.parse(icalData);
+  const comp = new ICAL.Component(jcalData);
+  const vevents = comp.getAllSubcomponents('vevent');
+
+  const now = new Date();
+  const sevenDaysFromNow = new Date();
+  sevenDaysFromNow.setDate(now.getDate() + 7);
+
+  return vevents.map(vevent => {
+    const event = new ICAL.Event(vevent);
+    return {
+      summary: event.summary,
+      startDate: event.startDate.toJSDate(),
+      endDate: event.endDate.toJSDate(),
+      description: event.description,
+      id: event.uid,
+    }
+  }).filter(event => event.startDate >= now && event.startDate <= sevenDaysFromNow)
 }
 
-async function findNotionItemByName(name) {
-  const response = await notion.databases.query({
-    database_id: databaseCoursId,
-  });
-  return response.results.find(item => 
-    item.url.includes(name.replace(/\s/g, '-').replace('\'', '-').replace('--', '-').toUpperCase())
-  );
-}
+async function getUpcomingEvents(calendarUrl) {
+  const res = await fetch(calendarUrl);
+  const icalData = await res.text();
 
-const extractSummary = (summary) => {
-  const match = summary.match(/; (.*?);/);
-  return match ? match[1] : '';
-};
+  const jcalData = ICAL.parse(icalData);
+  const comp = new ICAL.Component(jcalData);
+  const vevents = comp.getAllSubcomponents('vevent');
+
+  const now = new Date();
+  const sevenDaysFromNow = new Date();
+  sevenDaysFromNow.setDate(now.getDate() + 7);
+
+  return vevents.map(vevent => {
+    const event = new ICAL.Event(vevent);
+    return {
+      course: extractSummary(event.summary),
+      start: event.startDate.toJSDate(),
+      end: event.endDate.toJSDate(),
+      location: event.location,
+      description: parseDescription(event.description || ''),
+    };
+  })
+  .filter(event => event.start >= now && event.start <= sevenDaysFromNow)
+  .sort((a, b) => a.start < b.start ? 1 : -1);
+}
 
 const parseDescription = (description) => {
   const categories = description.match(/Catégorie: (.+)/);
@@ -50,7 +64,7 @@ const parseDescription = (description) => {
   const desc = description.match(/Description: (.+)/);
 
   return {
-    categorie: categories ? categories[1] : '',
+    categorie: categories ? new Array(categories[1]) : [],
     tuteurs: tuteurs ? tuteurs[1].split(';').map(t => t.trim()) : [],
     groupes: groupes ? groupes[1].split(';').map(g => g.trim()) : [],
     salles: salle ? salle[1].split(';').map(s => s.trim()) : [],
@@ -58,65 +72,98 @@ const parseDescription = (description) => {
   };
 };
 
-async function processEvent(eventInfo, lastEvent) {
-  if (eventInfo.categorie === '' || eventInfo.tuteurs.length === 0 || eventInfo.groupes.length === 0 || eventInfo.salles.length === 0 || eventInfo.description === '') {
+const extractSummary = (summary) => {
+  const match = summary.match(/; (.*?);/);
+  return match ? match[1] : '';
+};
+
+async function findNotionItemByName(name, databaseId) {
+  const response = await notion.databases.query({
+    database_id: databaseId,
+  });
+  return response.results.find(item => 
+    item.url.includes(name.replace(/\s/g, '-').replace('\'', '-').replace('--', '-').toUpperCase())
+  );
+}
+
+
+async function calendarProcessEvent(eventInfo, lastEvent) {
+  if (eventInfo.description.categorie === '' || eventInfo.description.tuteurs.length === 0 || eventInfo.description.groupes.length === 0 || eventInfo.description.salles.length === 0 || eventInfo.description.description === '') {
     console.info('missing information !');
     return;
   }
 
-  if (lastEvent && lastEvent.summary === eventInfo.summary) {
+  if (lastEvent && lastEvent.course === eventInfo.course) {
     const timeMarge = new Date(lastEvent.start).getTime() - new Date(eventInfo.end).getTime();
-    console.log('time marge : ' + timeMarge + ' ms for event ' + eventInfo.summary + ' and last event ' + lastEvent.summary);
+    console.log('Marge de temps ' + timeMarge + ' ms pour le cours "' + eventInfo.course + '", et le cours précédent "' + lastEvent.course + '"');
 
-    if (timeMarge <= 900000) {
+    if (timeMarge <= 900000 && timeMarge >= -900000) {
       eventInfo.end = lastEvent.end;
-      eventInfo.tuteurs = [...new Set([...eventInfo.tuteurs, ...lastEvent.tuteurs])];
-      eventInfo.groupes = [...new Set([...eventInfo.groupes, ...lastEvent.groupes])];
-      eventInfo.salles = [...new Set([...eventInfo.salles, ...lastEvent.salles])];
-      eventInfo.description = eventInfo.description + '\n' + lastEvent.description + '\n\n Cours fusionnés';
-      console.info('events merged !');
+      eventInfo.description.tuteurs = [...new Set([...eventInfo.description.tuteurs, ...lastEvent.description.tuteurs])];
+      eventInfo.description.categorie = [...new Set([...eventInfo.description.categorie, ...lastEvent.description.categorie])];
+      eventInfo.description.groupes = [...new Set([...eventInfo.description.groupes, ...lastEvent.description.groupes])];
+      eventInfo.description.salles = [...new Set([...eventInfo.description.salles, ...lastEvent.description.salles])];
+      eventInfo.description.description = eventInfo.description.description + '\n' + lastEvent.description.description + '\n\n Cours fusionnés';
+      console.info('Cours fusionnés !');
+    } else {
+      console.log('Cours non fusionnés !');
     }
   }
 
-  const item = await findNotionItemByName(eventInfo.summary);
+  const item = await findNotionItemByName(eventInfo.course, process.env.NOTION_DATABASE_COURS_ID);
   if (item) {
-    console.info('item found !');
-    await notion.pages.update({
-      page_id: item.id,
-      properties: {
-        Date: {
-          "id": "M%3BBw",
-          "type": "date",
-          "date": {
-            "start": eventInfo.start,
-            "end": eventInfo.end,
-            "time_zone": null
-          }
+    console.info('Cours ' + eventInfo.course + ' trouvé ! Vérification des informations avant mise à jour...');
+    const properties = item.properties;
+    const date = properties.Date.date.start;
+    const type = properties.Type.multi_select.map(t => t.name);
+    const prof = properties.Prof.multi_select.map(t => t.name);
+    const groupes = properties.Groupes.multi_select.map(t => t.name);
+    const salle = properties.Salle.multi_select.map(t => t.name);
+    const description = properties.Description.rich_text.map(t => t.text.content).join('');
+
+    if (new Date(date).getTime() === new Date(eventInfo.start).getTime() && type.includes(eventInfo.description.categorie) && prof.every(p => eventInfo.description.tuteurs.includes(p)) && groupes.every(g => eventInfo.description.groupes.includes(g)) && salle.every(s => eventInfo.description.salles.includes(s)) && description === eventInfo.description.description) {
+      console.info('Le cours de "' + eventInfo.course + '" est déjà à jour ! Passage au cours suivant...');
+      return;
+    } else {
+      console.info('Le cours de "' + eventInfo.course + '" n\'est pas à jour ! Mise à jour en cours...');
+
+      await notion.pages.update({
+        page_id: item.id,
+        properties: {
+          Date: {
+            "id": "M%3BBw",
+            "type": "date",
+            "date": {
+              "start": eventInfo.start,
+              "end": eventInfo.end,
+              "time_zone": null
+            }
+          },
+          Type: {
+            multi_select: eventInfo.description.categorie.map(c => ({ name: c })),
+          },
+          Prof: {
+            multi_select: eventInfo.description.tuteurs.map(t => ({ name: t })),
+          },
+          Groupes: {
+            multi_select: eventInfo.description.groupes.map(g => ({ name: g })),
+          },
+          Salle: {
+            multi_select: eventInfo.description.salles.map(s => ({ name: s })),
+          },
+          Description: {
+            rich_text: [{ text: { content: eventInfo.description.description } }],
+          },
         },
-        Type: {
-          multi_select: [{ name: eventInfo.categorie }],
-        },
-        Prof: {
-          multi_select: eventInfo.tuteurs.map(t => ({ name: t })),
-        },
-        Groupes: {
-          multi_select: eventInfo.groupes.map(g => ({ name: g })),
-        },
-        Salle: {
-          multi_select: eventInfo.salles.map(s => ({ name: s })),
-        },
-        Description: {
-          rich_text: [{ text: { content: eventInfo.description } }],
-        },
-      },
-    });
-    console.info('item updated !');
+      });
+    }
+    console.info('Cours ' + eventInfo.course + ' mis à jour !');
   } else {
-    console.info('item not found ! creating item...');
+    console.info('Cours ' + eventInfo.course + ' non trouvé ! Création...');
     await notion.pages.create({
-      parent: { type: 'database_id', database_id: databaseCoursId },
+      parent: { type: 'database_id', database_id: process.env.NOTION_DATABASE_COURS_ID },
       properties: {
-        title: { title: [{ text: { content: eventInfo.summary } }] },
+        title: { title: [{ text: { content: eventInfo.course } }] },
         Date: {
           "id": "M%3BBw",
           "type": "date",
@@ -127,100 +174,124 @@ async function processEvent(eventInfo, lastEvent) {
           }
         },
         Type: {
-          multi_select: [{ name: eventInfo.categorie }],
+          multi_select: [{ name: eventInfo.description.categorie }],
         },
         Prof: {
-          multi_select: eventInfo.tuteurs.map(t => ({ name: t })),
+          multi_select: eventInfo.description.tuteurs.map(t => ({ name: t })),
         },
         Groupes: {
-          multi_select: eventInfo.groupes.map(g => ({ name: g })),
+          multi_select: eventInfo.description.groupes.map(g => ({ name: g })),
         },
         Salle: {
-          multi_select: eventInfo.salles.map(s => ({ name: s })),
+          multi_select: eventInfo.description.salles.map(s => ({ name: s })),
         },
         Description: {
-          rich_text: [{ text: { content: eventInfo.description } }],
+          rich_text: [{ text: { content: eventInfo.description.description } }],
         },
       },
     });
-    console.info('item created !');
+    console.info('Nouveau cours, ' + eventInfo.course + ' ajouté !');
   }
 }
 
-async function main() {
-  try {
-    const events = await getUpcomingEvents();
-    const eventInfos = events.map(event => ({
-      summary: extractSummary(event.summary),
-      location: event.location,
-      start: event.start.dateTime,
-      end: event.end.dateTime,
-      ...parseDescription(event.description || ''),
-    }));
+// async function moodleProcessEvent(eventInfo) {
+//   const item = await findNotionItemByName(eventInfo.summary, process.env.NOTION_DATABASE_TASKS_ID);
 
-    console.log(eventInfos.length + ' events found !');
+//   if(item) {
+//     console.info('Tâche ' + eventInfo.summary + ' trouvée ! Vérification des informations avant mise à jour...');
+//     const properties = item.properties;
+//     const date = properties.Date.date.start;
+//     const description = properties.Description.rich_text.map(t => t.text.content).join('');
+   
+//     if (new Date(date).getTime() === new Date(eventInfo.startDate).getTime() && description === eventInfo.description) {
+//       console.info('La tâche de "' + eventInfo.summary + '" est déjà à jour ! Passage à la tâche suivante...');
+//       return;
+//     } else {
+//       console.info('La tâche de "' + eventInfo.summary + '" n\'est pas à jour ! Mise à jour en cours...');
+
+//       await notion.pages.update({
+//         page_id: item.id,
+//         properties: {
+//           Date: {
+//             "id": "M%3BBw",
+//             "type": "date",
+//             "date": {
+//               "start": eventInfo.startDate,
+//               "end": eventInfo.endDate,
+//               "time_zone": null
+//             }
+//           },
+//           Description: {
+//             rich_text: [{ text: { content: eventInfo.description } }],
+//           },
+//         },
+//       });
+//       console.info('Tâche ' + eventInfo.summary + ' mise à jour !');
+//     }
+//   } else {
+//     await notion.pages.create({
+//       parent: { type: 'database_id', database_id: process.env.NOTION_DATABASE_TASKS_ID },
+//       properties: {
+//         title: { title: [{ text: { content: eventInfo.summary } }] },
+//         Date: {
+//           "id": "M%3BBw",
+//           "type": "date",
+//           "date": {
+//             "start": eventInfo.startDate,
+//             "end": eventInfo.endDate,
+//             "time_zone": null
+//           }
+//         },
+//         Description: {
+//           rich_text: [{ text: { content: eventInfo.description } }],
+//         },
+//         id: {
+//           rich_text: [{ text: { content: eventInfo.id } }],
+//         }
+//       },
+//     });
+//     console.info('Nouvelle tâche, ' + eventInfo.summary + ' ajoutée !');
+//   }
+// }
+
+// async function mainMoodle() {
+//   try {
+//     const tasks = await getUpcomingEventsForMoodle(process.env.MOODLE_CALENDAR_URL);
+
+//     console.log(tasks.length + ' events found !');
+
+//     for(const tasksInfo of tasks) {
+//       await moodleProcessEvent(tasksInfo);
+//     }
+//     console.log('Toutes les tâches ont été traitées !');
+//   } catch (error) {
+//     console.error('Erreur:', error);
+//   }
+// }
+
+// mainMoodle();
+
+async function mainCalendar() {
+  try {
+    const events = await getUpcomingEvents(process.env.EDT_CALENDAR_URL);
+
+    console.log(events.length + ' cours trouvés !');
 
     let lastEvent = null;
-    for (const eventInfo of eventInfos) {
-      await processEvent(eventInfo, lastEvent);
+    for (const eventInfo of events) {
+      await calendarProcessEvent(eventInfo, lastEvent);
       lastEvent = eventInfo;
     }
+    console.log('Tous les cours ont été traités !');
   } catch (error) {
-    console.error('Error:', error);
+    console.error('Erreur:', error);
   }
-}
-
-function getMeals() {
-  const url = "https://api.repas.crous.alexis-rarchaert.fr/getRepas/r666"; //Résulats sous forme de JSON
-
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      //Check notion db if meal already exists for today
-      notion.databases.query({
-        database_id: databaseMealsId,
-      }).then(response => {
-        const today = new Date().toISOString().split('T')[0];
-        const meal = response.results.find(item => item.properties.Date.date.start === today);
-        if (!meal) {
-          console.log(data[0].entrees.map(e => ({ text: { content: e } })));
-          //Create new meal
-          notion.pages.create({
-            parent: { type: 'database_id', database_id: databaseMealsId },
-            properties: {
-              title: { title: [{ text: { content: today } }] },
-              Date: {
-                "id": "M%3BBw",
-                "type": "date",
-                "date": {
-                  "start": today,
-                  "end": today,
-                  "time_zone": null
-                }
-              },
-              Entrées: {
-                multi_select: data[0].entrees.map(e => ({ name: e })),
-              },
-              Plats: {
-                multi_select: data[0].plats.map(p => ({ name: p})),
-              },
-              Desserts: {
-                multi_select: data[0].desserts.map(d => ({ name: d})),
-              },
-            },
-          });
-        }
-      });
-    });
 }
 
 setInterval(() => {
   console.log('running script...');
   const hours = new Date().getHours();
   if (hours === 7 || hours === 13) {
-    main();
-  }
-  if(hours === 10) {
-    getMeals();
+    mainCalendar();
   }
 }, 60 * 60 * 1000);
